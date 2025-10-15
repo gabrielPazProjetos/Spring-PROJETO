1de6c8e84e6451e717d29ef69b3e4468c3c735c4
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-10-14 23:34:43 -0300

    Update DemoApplication.java

--- src/main/java/com/gabriel/demo/DemoApplication.java
+++ src/main/java/com/gabriel/demo/DemoApplication.java
@@ -1,1 +1,15 @@
+package com.gabriel.demo;
 
+// Importa as classes necessárias do Spring Boot
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Essa anotação ativa o auto-configuração do Spring Boot e marca a classe como ponto de partida
+@SpringBootApplication
+public class DemoApplication {
+
+    // Método principal que inicia a aplicação
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
